ab1f4ed285ad28bc743045541e228b26f2e200b4
Provash <provash20cb@gmail.com>
2025-09-17 20:43:12 +0600

    37-6 (Recap) Load dynamic data, API call using use()

--- src/Users.jsx
+++ src/Users.jsx
@@ -1,7 +1,13 @@
-export default function Users(){
+import { use } from "react"
+
+export default function Users({fetchUsers}){
+
+    const users = use(fetchUsers);
+    console.log(users); //loading checked ok
+
     return(
         <div className="card">
-        <h3>Users : </h3>
+        <h3>Users :{users.length} </h3>
         </div>
     )
 }
